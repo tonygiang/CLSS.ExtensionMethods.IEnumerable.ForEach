b2a450f753f3dddb0304aee763433603f2e6120d
tonygiang <tony_s_giang@yahoo.com>
2022-07-27 14:36:21 +0700

    Project push.

---
+++ IEnumerable.ForEach.cs
@@ -0,0 +1,322 @@
+﻿// A part of the C# Language Syntactic Sugar suite.
+
+using System;
+using System.Collections.Generic;
+
+namespace CLSS
+{
+  public static partial class IEnumerableForEach
+  {
+    /// <summary>
+    /// Performs the specified action on each element of the
+    /// <see cref="IEnumerable{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of <see cref="IEnumerable{T}"/> to iterate
+    /// action on.</typeparam>
+    /// <typeparam name="TElement">The type of the elements of
+    /// <paramref name="source"/>.</typeparam>
+    /// <param name="source">The <see cref="IEnumerable{T}"/> to iterate action
+    /// on.</param>
+    /// <param name="action">The <see cref="Action{T}"/> delegate to perform on
+    /// each element of the <see cref="IEnumerable{T}"/>.</param>
+    /// <returns>The source collection.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or
+    /// <paramref name="action"/> is null.</exception>
+    public static T ForEach<T, TElement>(this T source,
+      Action<TElement> action)
+      where T : IEnumerable<TElement>
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (action == null) throw new ArgumentNullException("action");
+      foreach (var e in source) action(e);
+      return source;
+    }
+
+    /// <summary>
+    /// Performs the specified action on each element of the
+    /// <see cref="IEnumerable{T}"/>.
+    /// </summary>
+    /// <typeparam name="TElement">The type of the elements of
+    /// <paramref name="source"/>.</typeparam>
+    /// <param name="source">The <see cref="IEnumerable{T}"/> to iterate action
+    /// on.</param>
+    /// <param name="action">The <see cref="Action{T}"/> delegate to perform on
+    /// each element of the <see cref="IEnumerable{T}"/>.</param>
+    /// <returns>The source collection.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or
+    /// <paramref name="action"/> is null.</exception>
+    public static IEnumerable<TElement> ForEach<TElement>(
+      this IEnumerable<TElement> source,
+      Action<TElement> action)
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (action == null) throw new ArgumentNullException("action");
+      foreach (var e in source) action(e);
+      return source;
+    }
+
+    /// <summary>
+    /// Performs the specified function on each element of the
+    /// <see cref="IEnumerable{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of <see cref="IEnumerable{T}"/> to iterate
+    /// function on.</typeparam>
+    /// <typeparam name="TElement">The type of the elements of
+    /// <paramref name="source"/>.</typeparam>
+    /// <param name="source">The <see cref="IEnumerable{T}"/> to iterate action
+    /// on.</param>
+    /// <param name="func">The <see cref="Func{T}"/> delegate to perform on
+    /// each element of the <see cref="IEnumerable{T}"/>.</param>
+    /// <returns>The source collection.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or
+    /// <paramref name="func"/> is null.</exception>
+    public static T ForEach<T, TElement, TR>(this T source,
+      Func<TElement, TR> func)
+      where T : IEnumerable<TElement>
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (func == null) throw new ArgumentNullException("func");
+      foreach (var e in source) func(e);
+      return source;
+    }
+
+    /// <summary>
+    /// Performs the specified function on each element of the
+    /// <see cref="IEnumerable{T}"/>.
+    /// </summary>
+    /// <typeparam name="TElement">The type of the elements of
+    /// <paramref name="source"/>.</typeparam>
+    /// <param name="source">The <see cref="IEnumerable{T}"/> to iterate action
+    /// on.</param>
+    /// <param name="func">The <see cref="Func{T}"/> delegate to perform on
+    /// each element of the <see cref="IEnumerable{T}"/>.</param>
+    /// <returns>The source collection.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or
+    /// <paramref name="func"/> is null.</exception>
+    public static IEnumerable<TElement> ForEach<TElement, TR>(
+      this IEnumerable<TElement> source,
+      Func<TElement, TR> func)
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (func == null) throw new ArgumentNullException("func");
+      foreach (var e in source) func(e);
+      return source;
+    }
+
+    /// <summary>
+    /// Performs the specified action on each element of the
+    /// <see cref="IEnumerable{T}"/>, passing through the element's index.
+    /// </summary>
+    /// <typeparam name="T">The type of <see cref="IEnumerable{T}"/> to iterate
+    /// action on.</typeparam>
+    /// <typeparam name="TElement">The type of the elements of
+    /// <paramref name="source"/>.</typeparam>
+    /// <param name="source">The <see cref="IEnumerable{T}"/> to iterate action
+    /// on.</param>
+    /// <param name="action">The <see cref="Action{T1, T2}"/> delegate to
+    /// perform on each element of the <see cref="IEnumerable{T}"/>, with its
+    /// second argument being the element's index.</param>
+    /// <returns>The source collection.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or
+    /// <paramref name="action"/> is null.</exception>
+    public static T ForEach<T, TElement>(this T source,
+      Action<TElement, int> action)
+      where T : IEnumerable<TElement>
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (action == null) throw new ArgumentNullException("action");
+      int index = 0;
+      foreach (var e in source) { action(e, index); ++index; }
+      return source;
+    }
+
+    /// <summary>
+    /// Performs the specified action on each element of the
+    /// <see cref="IEnumerable{T}"/>, passing through the element's index.
+    /// </summary>
+    /// <typeparam name="TElement">The type of the elements of
+    /// <paramref name="source"/>.</typeparam>
+    /// <param name="source">The <see cref="IEnumerable{T}"/> to iterate action
+    /// on.</param>
+    /// <param name="action">The <see cref="Action{T1, T2}"/> delegate to
+    /// perform on each element of the <see cref="IEnumerable{T}"/>, with its
+    /// second argument being the element's index.</param>
+    /// <returns>The source collection.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or
+    /// <paramref name="action"/> is null.</exception>
+    public static IEnumerable<TElement> ForEach<TElement>(
+      this IEnumerable<TElement> source,
+      Action<TElement, int> action)
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (action == null) throw new ArgumentNullException("action");
+      int index = 0;
+      foreach (var e in source) { action(e, index); ++index; }
+      return source;
+    }
+
+    /// <summary>
+    /// Performs the specified function on each element of the
+    /// <see cref="IEnumerable{T}"/>, passing through the element's index.
+    /// </summary>
+    /// <typeparam name="T">The type of <see cref="IEnumerable{T}"/> to iterate
+    /// action on.</typeparam>
+    /// <typeparam name="TElement">The type of the elements of
+    /// <paramref name="source"/>.</typeparam>
+    /// <param name="source">The <see cref="IEnumerable{T}"/> to iterate action
+    /// on.</param>
+    /// <param name="func">The <see cref="Func{T1, T2}"/> delegate to
+    /// perform on each element of the <see cref="IEnumerable{T}"/>, with its
+    /// second argument being the element's index.</param>
+    /// <returns>The source collection.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or
+    /// <paramref name="func"/> is null.</exception>
+    public static T ForEach<T, TElement, TR>(this T source,
+      Func<TElement, int, TR> func)
+      where T : IEnumerable<TElement>
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (func == null) throw new ArgumentNullException("func");
+      int index = 0;
+      foreach (var e in source) { func(e, index); ++index; }
+      return source;
+    }
+
+    /// <summary>
+    /// Performs the specified function on each element of the
+    /// <see cref="IEnumerable{T}"/>, passing through the element's index.
+    /// </summary>
+    /// <typeparam name="TElement">The type of the elements of
+    /// <paramref name="source"/>.</typeparam>
+    /// <param name="source">The <see cref="IEnumerable{T}"/> to iterate action
+    /// on.</param>
+    /// <param name="func">The <see cref="Func{T1, T2}"/> delegate to
+    /// perform on each element of the <see cref="IEnumerable{T}"/>, with its
+    /// second argument being the element's index.</param>
+    /// <returns>The source collection.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or
+    /// <paramref name="func"/> is null.</exception>
+    public static IEnumerable<TElement> ForEach<TElement, TR>(
+      this IEnumerable<TElement> source,
+      Func<TElement, int, TR> func)
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (func == null) throw new ArgumentNullException("func");
+      int index = 0;
+      foreach (var e in source) { func(e, index); ++index; }
+      return source;
+    }
+
+    /// <summary>
+    /// Performs the specified action on each element of the
+    /// <see cref="IEnumerable{T}"/>, passing through the element's index and
+    /// the collection being iterated on.
+    /// </summary>
+    /// <typeparam name="T">The type of <see cref="IEnumerable{T}"/> to iterate
+    /// action on.</typeparam>
+    /// <typeparam name="TElement">The type of the elements of
+    /// <paramref name="source"/>.</typeparam>
+    /// <param name="source">The <see cref="IEnumerable{T}"/> to iterate action
+    /// on.</param>
+    /// <param name="action">The <see cref="Action{T1, T2, T3}"/> delegate to
+    /// perform on each element of the <see cref="IEnumerable{T}"/>, with its
+    /// second argument being the element's index and the third argument being
+    /// <paramref name="source"/>.</param>
+    /// <returns>The source collection.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or
+    /// <paramref name="action"/> is null.</exception>
+    public static T ForEach<T, TElement>(this T source,
+      Action<TElement, int, T> action)
+      where T : IEnumerable<TElement>
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (action == null) throw new ArgumentNullException("action");
+      int index = 0;
+      foreach (var e in source) { action(e, index, source); ++index; }
+      return source;
+    }
+
+    /// <summary>
+    /// Performs the specified action on each element of the
+    /// <see cref="IEnumerable{T}"/>, passing through the element's index and
+    /// the collection being iterated on.
+    /// </summary>
+    /// <typeparam name="TElement">The type of the elements of
+    /// <paramref name="source"/>.</typeparam>
+    /// <param name="source">The <see cref="IEnumerable{T}"/> to iterate action
+    /// on.</param>
+    /// <param name="action">The <see cref="Action{T1, T2, T3}"/> delegate to
+    /// perform on each element of the <see cref="IEnumerable{T}"/>, with its
+    /// second argument being the element's index and the third argument being
+    /// <paramref name="source"/>.</param>
+    /// <returns>The source collection.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or
+    /// <paramref name="action"/> is null.</exception>
+    public static IEnumerable<TElement> ForEach<TElement>(
+      this IEnumerable<TElement> source,
+      Action<TElement, int, IEnumerable<TElement>> action)
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (action == null) throw new ArgumentNullException("action");
+      int index = 0;
+      foreach (var e in source) { action(e, index, source); ++index; }
+      return source;
+    }
+
+    /// <summary>
+    /// Performs the specified function on each element of the
+    /// <see cref="IEnumerable{T}"/>, passing through the element's index and
+    /// the collection being iterated on.
+    /// </summary>
+    /// <typeparam name="T">The type of <see cref="IEnumerable{T}"/> to iterate
+    /// action on.</typeparam>
+    /// <typeparam name="TElement">The type of the elements of
+    /// <paramref name="source"/>.</typeparam>
+    /// <param name="source">The <see cref="IEnumerable{T}"/> to iterate action
+    /// on.</param>
+    /// <param name="func">The <see cref="Func{T1, T2, T3}"/> delegate to
+    /// perform on each element of the <see cref="IEnumerable{T}"/>, with its
+    /// second argument being the element's index and the third argument being
+    /// <paramref name="source"/>.</param>
+    /// <returns>The source collection.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or
+    /// <paramref name="func"/> is null.</exception>
+    public static T ForEach<T, TElement, TR>(this T source,
+      Func<TElement, int, T, TR> func)
+      where T : IEnumerable<TElement>
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (func == null) throw new ArgumentNullException("func");
+      int index = 0;
+      foreach (var e in source) { func(e, index, source); ++index; }
+      return source;
+    }
+
+    /// <summary>
+    /// Performs the specified function on each element of the
+    /// <see cref="IEnumerable{T}"/>, passing through the element's index and
+    /// the collection being iterated on.
+    /// </summary>
+    /// <typeparam name="TElement">The type of the elements of
+    /// <paramref name="source"/>.</typeparam>
+    /// <param name="source">The <see cref="IEnumerable{T}"/> to iterate action
+    /// on.</param>
+    /// <param name="func">The <see cref="Func{T1, T2, T3}"/> delegate to
+    /// perform on each element of the <see cref="IEnumerable{T}"/>, with its
+    /// second argument being the element's index and the third argument being
+    /// <paramref name="source"/>.</param>
+    /// <returns>The source collection.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or
+    /// <paramref name="func"/> is null.</exception>
+    public static IEnumerable<TElement> ForEach<T, TElement, TR>(
+      this IEnumerable<TElement> source,
+      Func<TElement, int, IEnumerable<TElement>, TR> func)
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (func == null) throw new ArgumentNullException("func");
+      int index = 0;
+      foreach (var e in source) { func(e, index, source); ++index; }
+      return source;
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,90 @@
+﻿# CLSS.ExtensionMethods.IEnumerable.ForEach
+
+### Problem
+
+[`ForEach`](https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1.foreach) is a convenient method for `List<T>`. It provides a syntax similar to JavaScript's [`forEach`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach) array method. However, by default, this method:
+
+- Is not available to other `IEnumerable<T>` collections, even though by nature they can be iterated through a `foreach` loop.
+- Returns void and therefore not very friendly to a functional-style call chain.
+- Does not accept impure methods.
+- Does not support passing through each element's index and the original collection like JavaScript's `forEach` array method.
+
+### Solution
+
+This package provides `ForEach` for all `IEnumerable<T>` collections to address all of the above issues`.
+
+Using `ForEach` with `HashSet`:
+
+```
+using CLSS;
+
+var uniqueIDs = new HashSet<int>() { 2, 4, 15, 22 };
+uniqueIDs.ForEach(id => Console.WriteLine($"id: {id}"));
+```
+`ForEach` returns the source collection to continue to be used in a functional-style call chain:
+
+```
+using CLSS;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+
+var itemNodes = JArray.Parse(rawJSON).Properties()
+  .Select(p => p.Value)
+  .Cast<JObject>()
+  .ForEach(itemNode => UpdateItemLabel(itemNode))
+  .ToList();
+```
+
+Using `ForEach` with impure methods:
+
+```
+using CLSS;
+using UnityEngine;
+
+// In Unity's Scripting API, AssetBundle.LoadAssetAsync returns an
+// AssetBundleRequest.
+assetNames.ForEach(AssetBundle.LoadAssetAsync);
+```
+
+Using `ForEach` with JavaScript-style optional arguments:
+
+```
+using CLSS;
+
+(new float[] { 9.5f, 23.6f, 5.0f, 14.1f })
+  .ForEach<float[], float>((n, i, srcNumbers) =>
+  {
+    float sumWithNeighbours = n;
+    if (i > 0) sumWithNeighbours += srcNumbers[i - 1];
+    if (i < srcNumbers.Length - 1) sumWithNeighbours += srcNumbers[i + 1];
+    Console.WriteLine(sumWithNeighbours);
+  });
+```
+
+The exact return type of `ForEach` will be determined by the invocation syntax. Below is how return type rules apply:
+
+```
+using CLSS;
+
+var numbers = new float[] { 0.0f, 1.0f, 2.0f };
+
+// Implicit invocation and implicit lambda expression return IEnumerable<float>
+numbers.ForEach(n => { }); // returns IEnumerable<float>
+// Explicit invocation and implicit lambda expression return original type
+numbers.ForEach<float[], float>(n => { }); // returns float[];
+// Implicit invocation and explicit lambda expression return original type
+numbers.ForEach((float n) => { }); // returns float[];
+// Implicit invocation and delegate instance return original type
+Action<float> ProcessNumber = n => { };
+numbers.ForEach(ProcessNumber}); // returns float[];
+// Implicit invocation and method reference return IEnumerable<float>
+static void ProcessNumber(float n) { }
+numbers.ForEach(ProcessNumber}); // returns IEnumerable<float>;
+// Explicit invocation and method reference return original type
+static void ProcessNumber(float n) { }
+numbers.ForEach<float[], float>(ProcessNumber}); // returns float[];
+```
+
+Note that using lambdas in lieu of writing plain `foreach` loops will incur additional memory allocations. `ForEach` extension methods - just as with `List<T>.ForEach` - are meant to be syntactic sugar only. Only use it in performance-critical code paths if you can afford the added overhead.
+
+##### This package is a part of the [C# Language Syntactic Sugar suite](https://github.com/tonygiang/CLSS).
